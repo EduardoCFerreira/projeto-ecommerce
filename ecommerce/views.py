00956823c89a1573a2c9479d7bb31575f2d9205d
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-08-01 20:25:11 -0300

    fazendo a barra de pesquisa

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'ecommerce/home.html')
+    return render(request, 'ecommerce/pages/home.html')
