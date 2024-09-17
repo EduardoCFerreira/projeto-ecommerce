77cff2d6423be1c568b9e00d9c4f9705d2b53e10
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-09-17 02:39:27 -0300

    Criando os models de Pedido e Item pedido

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pedido(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices= (
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__ (self):
+        return f'Pedido N. {self.pk}'
+    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ <-- Está retornando uma String e o id do pedido
+        
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.CharField(max_length=255)
+    produto_id = models.PositiveIntegerField()
+    variacao = models.CharField(max_length=255)
+    variacao_id = models.PositiveIntegerField()
+    preco = models.FloatField()
+    preco_promopcional = models.FloatField(default=0)
+    quantidade = models.PositiveIntegerField()
+    imagem = models.CharField(max_length=3000)
+
+    def __str__(self):
+        return f'Item do {self.pedido}'
+    # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ <-- Está retornando uma String e o nome do pedido
+
+    class Meta:
+#   ^^^^^^^^^^ <-- Essa class Meta tem a função de alterar o nome da tabela do banco de dados
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Itens do pedido'
+
+    
