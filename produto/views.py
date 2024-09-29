873103cc0e1b6b0b1e8c7fa95505e67778af4de9
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-09-29 05:17:15 -0300

    fazendo a tela de detalhe do produto

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -9,9 +10,11 @@ class ListaProdutos(ListView):
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
