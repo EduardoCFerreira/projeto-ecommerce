b644fe9474440ab172c6bffca9c2ced490f1c0b4
Eduardo Cales Ferreira <eduardoc.ferreira0901@gmail.com>
2024-09-29 12:41:53 -0300

    arrumei denovo

--- produto/urls.py
+++ produto/urls.py
@@ -5,8 +5,8 @@ app_name = 'produto'
 
 urlpatterns = [
     path('', views.ListaProdutos.as_view(), name='lista'),
-    path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
-    path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
+    path('<slug>/', views.DetalheProduto.as_view(), name='detalhe'),
+    path('adicionaraocarrinho', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
     path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
     path('carrinho/', views.Carrinho.as_view(), name='carrinho'),
     path('finalizar/', views.Finalizar.as_view(), name='finalizar'),
